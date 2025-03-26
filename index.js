3141f2e316e6b6c52cf0c3a47f569a653ee62a03
Aimi Shaqirah <aimishaqirah33@gmail.com>
2025-03-27 00:26:54 +0800

    Updated index.js and modified .gitignore

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ async function main() {
         const collection = db.collection("users");
 
         // Insert a document
-        await collection.insertOne({ name: "Aimi", age: 21 });
+        await collection.insertOne({ name: "Nina", age: 21 });
         console.log("Document inserted!");
 
         // Query the document
-        const result = await collection.findOne({ name: "Aimi" });
+        const result = await collection.findOne({ name: "Nina" });
         console.log("Query result:", result);
     } catch (err) {
         console.error("Error:", err);
